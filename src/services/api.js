028753fd3b34bc31547a4e2f38da8c8f93cca308
Rozi19 <rozaalene@gmail.com>
2026-06-03 07:43:23 -0700

    Update API URL for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://trade-ethiopia-task-manager-backend-7ntp.onrender.com",
 });
 
 export default api;
