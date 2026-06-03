990b849ee00ebeff450462d933a569e89b383133
Rozi19 <rozaalene@gmail.com>
2026-06-03 08:36:05 -0700

    add http

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://trade-ethiopia-task-manager-backend-7ntp.onrender.com",
+  baseURL: "https://trade-ethiopia-task-manager-backend-7ntp.onrender.com/api",
 });
 
 export default api;
